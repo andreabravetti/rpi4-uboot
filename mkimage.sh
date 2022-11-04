99dc5d7940e44f03da9ab7a07ec04f9dd4e567e3
Andrea Bravetti <a.bravetti@teamsystem.com>
2022-11-04 15:00:56 +0100

    Update mkimage.sh to use flash-kernel functions

--- mkimage.sh
+++ mkimage.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-mkimage -A arm64 -T script -C none -n "Boot script" -d bootscr.rpi build/boot.scr
-mkimage -A arm64 -T script -C none -n "Boot script" -d bootscr-tpm.rpi build/boot.scr.uimg
+#mkimage -A arm64 -T script -C none -n "Boot script" -d bootscr.rpi build/boot.scr
+#mkimage -A arm64 -T script -C none -n "Boot script" -d bootscr-tpm.rpi build/boot.scr.uimg
+
+set -e
+
+FK_DIR="/usr/share/flash-kernel"
+
+. "${FK_CHECKOUT:-$FK_DIR}/functions"
+
+mkimage_script "boot" "script" "bootscr-tpm.rpi" "build/boot.scr.uimg"
 
